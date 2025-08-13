b28a3439e1920b8758da24485adc8279d6fe3125
JaPNaA <japnaa@japnaa.dev>
2025-08-12 20:59:50 -0400

    Add index first line as directory link text option

--- src/auto_gen_summary/config.rs
+++ src/auto_gen_summary/config.rs
@@ -8,11 +8,13 @@ use crate::auto_gen_summary::PREPROCESSOR_NAME;
 const README_FILE: &str = "README.md";
 
 const OPT_FIRST_LINE_AS_LINK: &str = "first-line-as-link-text";
+const OPT_INDEX_FIRST_LINE_AS_DIRECTORY_LINK: &str = "index-first-line-as-directory-link-text";
 const OPT_GENERATE_STUB_DIRECTORY_INDEX: &str = "generate-stub-directory-index";
 const OPT_DIRECTORY_INDEX_NAMES: &str = "directory-index-names";
 
 pub struct AutoGenConfig {
     pub first_line_as_link_text: bool,
+    pub index_first_line_as_directory_link_text: bool,
     pub generate_stub_directory_index: bool,
     pub directory_index_names: HashSet<String>,
 }
@@ -21,6 +23,7 @@ impl AutoGenConfig {
     pub fn new() -> AutoGenConfig {
         AutoGenConfig {
             first_line_as_link_text: false,
+            index_first_line_as_directory_link_text: false,
             generate_stub_directory_index: false,
             directory_index_names: {
                 let mut s = HashSet::new();
@@ -39,8 +42,12 @@ impl AutoGenConfig {
             self.first_line_as_link_text = v.as_bool().unwrap_or(false);
         }
 
-        if let Some(v) = cfg.get(OPT_FIRST_LINE_AS_LINK) {
-            self.first_line_as_link_text = v.as_bool().unwrap_or(false);
+        if let Some(v) = cfg.get(OPT_INDEX_FIRST_LINE_AS_DIRECTORY_LINK) {
+            self.index_first_line_as_directory_link_text = v.as_bool().unwrap_or(false);
+        }
+
+        if let Some(v) = cfg.get(OPT_GENERATE_STUB_DIRECTORY_INDEX) {
+            self.generate_stub_directory_index = v.as_bool().unwrap_or(false);
         }
 
         if let Some(v) = cfg.get(OPT_DIRECTORY_INDEX_NAMES) {
